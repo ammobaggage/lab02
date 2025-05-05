e44e18cba9aaa7446b615e74cd47928953a285ec
ammobaggage <karev.g.a@mail.ru>
2025-05-05 22:27:34 +0300

    Добавлен файл 1.2, без using namespace std;

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,10 +1,9 @@
 #include <iostream>
-using namespace std;
 
 int main() {
-	string name;
-	cout << "Введите свое имя:";
-	cin >> name;
-	cout << "Hello, World от " << name;
+	std::string name;
+	std::cout << "Введите свое имя:";
+	std::cin >> name;
+	std::cout << "Hello, World от " << name;
 	return 0;
 }
