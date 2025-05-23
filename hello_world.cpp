f502dcaec0cebd71b5f9dd45447f77d2f5f39e54
ammobaggage <karev.g.a@mail.ru>
2025-05-23 15:08:12 +0300

    Улучшен код программы

--- hello_world.cpp
+++ hello_world.cpp
@@ -2,6 +2,11 @@
 using namespace std;
 
 int main() {
-	cout << "Hello World!";
+
+	cout << "Hello world!" << endl;
+	cout << "Введите имя пользователя: ";
+	string name;
+	getline(cin, name);
+	cout << "Hello world from " << name <<  endl;
 	return 0;
 }
